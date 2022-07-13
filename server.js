223240d60d38b64400f4958f6404367e5a5197e3
Fabio Privitera <fabio_privitera@yahoo.it>
2022-07-13 08:55:50 +0300

    3 routes are available in the server: 1) /totaltours?month=<month> retrieve the right dataset; 2)/alltours just for testing(will be deleted);3)/page?month=<month>&page=<page> will find the right chuncks of data inside the specific dataset); This implementation is needed to avoid long waiting times when querying the database

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
 const database = require('./controller/controller_db');
+app.use(cors());
 app.use(express.json());
 //connecting
 app.listen(port, () => {
@@ -17,4 +18,44 @@ app.listen(port, () => {
       console.log('mySQL connection succesful');
     }
   });
+});
+//number of rows (use for pagination and better performance)
+app.route('/totaltours').get(async (req, res) => {
+  const month = req.query.month;
+  try {
+    //result will store the informations retrieved from the sql query (SELECT COUNT(id) as newid FROM may)
+    const result = await database.totalsofmonth(month);
+    console.log(result[0].newid);
+    //and send the response and the result to the client
+    res.status(200).json(result[0].newid);
+  } catch (err) {
+    res.status(404).end();
+  }
+});
+
+//find all
+app.route('/alltours').get(async (req, res) => {
+  try {
+    //result will store the informations retrieved from the sql query (SELECT * FROM may)
+    const result = await database.findAll();
+    console.log(result.length);
+    //and send the response and the result to the client
+    res.status(200).send(result);
+  } catch (err) {
+    res.status(404).end();
+  }
+});
+//implementing pagination
+
+app.route('/page').get(async (req, res) => {
+  console.log('inside page route');
+  const month = req.query.month;
+  const page = req.query.page;
+  console.log(page);
+  try {
+    const result = await database.findSinglepageOfData(page, month);
+    res.status(200).send(result);
+  } catch (err) {
+    res.status(404).end();
+  }
 });
