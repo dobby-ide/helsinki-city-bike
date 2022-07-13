094901a27996acb78f9a88af54cecb023004dc46
Fabio Privitera <fabio_privitera@yahoo.it>
2022-07-13 08:49:49 +0300

    controller_db.js now can count the  available rows in each datasets to better dividing each query page number button; can also find query SQL to find each single page of data in chuncks of one thousand: (1-999,1000-1999,...)

--- controller/controller_db.js
+++ controller/controller_db.js
@@ -18,16 +18,50 @@ module.exports = {
       }
     });
   },
+  //total numbers of row in may table
+  totalsofmonth: (month) => {
+    function myProm(resolve, reject) {
+      dbConnection.query(
+        `SELECT COUNT(id) as newid FROM ${month}`,
+        (err, results) => {
+          if (results) {
+            resolve(results);
+          } else {
+            reject(console.log(err));
+          }
+        }
+      );
+    }
+    return new Promise(myProm);
+  },
+
   //find all
   findAll: () => {
     function myProm(resolve, reject) {
-      dbConnection.query('SELECT * FROM may', (err, results) => {
+      dbConnection.query('SELECT * FROM may LIMIT 100', (err, results) => {
         if (results) {
           resolve(results);
         } else {
           reject(console.log(err));
         }
       });
+    }
+    return new Promise(myProm);
+  },
+
+  //load pages of data(pagination)
+  findSinglepageOfData: (page, month) => {
+    function myProm(resolve, reject) {
+      dbConnection.query(
+        `SELECT * FROM ${month} where id>${page}*1000 AND id < (${page} + 1)*1000`,
+        (err, results) => {
+          if (results) {
+            resolve(results);
+          } else {
+            reject(console.log('here'));
+          }
+        }
+      );
     }
     return new Promise(myProm);
   },
