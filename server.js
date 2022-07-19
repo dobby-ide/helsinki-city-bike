2bd12ccbdfc867d76f6e51bf11386a0037a2ab9e
Fabio Privitera <fabio_privitera@yahoo.it>
2022-07-19 19:41:04 +0300

    adds new routes to the server, and queries the controller accordingly: /sortedpagewithdays, /sortedpage, /allstations

--- server.js
+++ server.js
@@ -19,12 +19,35 @@ app.listen(port, () => {
     }
   });
 });
-//number of rows (use for pagination and better performance)
-app.route('/totaltours').get(async (req, res) => {
+//number of rows (use for pagination and better performance) in a SINGLE MONTH ONLY
+app.route('/everytoursofthemonth').get(async (req, res) => {
   const month = req.query.month;
+  console.log(month);
   try {
     //result will store the informations retrieved from the sql query (SELECT COUNT(id) as newid FROM may)
-    const result = await database.totalsofmonth(month);
+    const result = await database.totalsofonlymonth(month);
+
+    //and send the response and the result to the client
+    res.status(200).json(result[0].newid);
+  } catch (err) {
+    res.status(404).end();
+  }
+});
+//number of rows in case of filtered search
+app.route('/filteredtours').get(async (req, res) => {
+  const month =
+    req.query.month === 'may' ? '05' : req.query.month === 'june' ? '06' : '07';
+  const monthtable = req.query.month;
+
+  const departure = `2021-${month}-${req.query.start} 00:00:00`;
+  const returning = `2021-${month}-${req.query.end} 00:00:00`;
+  try {
+    //result will store the informations retrieved from the sql query (SELECT COUNT(id) as newid FROM may)
+    const result = await database.totalsofmonth(
+      monthtable,
+      departure,
+      returning
+    );
     console.log(result[0].newid);
     //and send the response and the result to the client
     res.status(200).json(result[0].newid);
@@ -55,6 +78,62 @@ app.route('/page').get(async (req, res) => {
   try {
     const result = await database.findSinglepageOfData(page, month);
     res.status(200).send(result);
+  } catch (err) {
+    res.status(404).end();
+  }
+});
+//sorting by distance or duration and days
+app.route('/sortedpagewithdays').get(async (req, res) => {
+  const month =
+    req.query.month === 'may' ? '05' : req.query.month === 'june' ? '06' : '07';
+  const monthtable = req.query.month;
+  const start = `2021-${month}-${req.query.start} 00:00:00`;
+  const end = `2021-${month}-${req.query.end} 00:00:00`;
+  const page = req.query.page;
+  const sort = req.query.sort;
+  console.log(page,month,start,end,sort);
+  try {
+    const result = await database.findSortedSinglepageOfData(
+      monthtable,
+      page,
+      sort,
+      start,
+      end
+    );
+    res.status(200).send(result);
+  } catch (err) {
+    res.status(404).end();
+  }
+});
+//1000 results: for SORT  
+app.route('/sortedpage').get(async (req, res) => {
+ 
+  const monthtable = req.query.month;
+  const page = req.query.page;
+  const sort = req.query.sort;
+  console.log(page);
+  try {
+   
+    const result = await database.sortbyandmonth(
+      monthtable,
+      page,
+      sort
+    );
+    res.status(200).send(result);
+  } catch (err) {
+    res.status(404).end();
+  }
+});
+
+///////////////////////////STATIONS STATIONS STATIONS STATIONS
+//retrieves all stations names
+app.route('/allstations').get(async (req, res) => {
+  try {
+    //result will store the informations retrieved from the sql query (SELECT * FROM stations)
+    const result = await database.findAllStations();
+    console.log(result.length);
+    //and send the response and the result to the client
+    res.status(200).json(result);
   } catch (err) {
     res.status(404).end();
   }
