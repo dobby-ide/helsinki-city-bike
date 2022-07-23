6f5933e7ef3a726847f8a636fb26bf8bf2de42e0
Fabio Privitera <fabio_privitera@yahoo.it>
2022-07-23 11:34:15 +0300

    StationsData: fetches and renders infor about the number of journeys for the selected station

--- frontend/src/components/StationsData.js
+++ frontend/src/components/StationsData.js
@@ -5,6 +5,9 @@ import axios from 'axios';
 const StationsData = ({ coords }) => {
   const [stations, setStations] = useState([]);
   const [searchField, setSearchField] = useState('');
+  const [departureNumber, setDepartureNumber] = useState();
+  const [arrivalNumber, setArrivalNumber] = useState();
+
   useEffect(() => {
     availableStations();
   }, []);
@@ -22,17 +25,32 @@ const StationsData = ({ coords }) => {
     setSearchField(searchFieldString);
   };
   const onRetrieveStationInformations = (e) => {
-    const coordinates = stations.find((x) => x.FID === Number(e.target.id));
+    const coordinates = stations.find((x) => x.ID === Number(e.target.id));
     console.log(coordinates);
     coords(coordinates.x_coord, coordinates.y_coord);
+    onStationsData(e.target.id);
   };
+  const onStationsData = async (fid) => {
+    const data = await axios.get(
+      `http://localhost:3000/infostation?fid=${fid}`
+    );
+    let stationInfo = data.data[0];
+    const numberOfDepartures = stationInfo[Object.keys(stationInfo)[0]];
+    let stationInfo2 = data.data[1];
+    const numberOfArrivals = stationInfo2[Object.keys(stationInfo2)[0]];
+
+    setArrivalNumber(numberOfArrivals);
+    setDepartureNumber(numberOfDepartures);
+    console.log(data.data);
+  };
+
   return (
     <div>
       <input type="search" onChange={onSearchChange}></input>
       {filteredStations.map((station) => {
         return (
           <div
-            id={station.FID}
+            id={station.ID}
             key={station.FID}
             onClick={onRetrieveStationInformations}
           >
@@ -41,6 +59,17 @@ const StationsData = ({ coords }) => {
           </div>
         );
       })}
+      {arrivalNumber ? (
+        <div>
+          <div>
+            total numbers of journeys from this station: {departureNumber}
+          </div>
+          <div>
+            total numbers of journeys to this station:
+            {arrivalNumber}
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
