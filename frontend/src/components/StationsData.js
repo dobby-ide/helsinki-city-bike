04e1aa6e34787e452f59f435ca98fbad4118670d
Fabio Privitera <fabio_privitera@yahoo.it>
2022-07-26 22:41:59 +0300

    StationsData.js: changes in class names (css)

--- frontend/src/components/StationsData.js
+++ frontend/src/components/StationsData.js
@@ -1,31 +1,24 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-// setAVGfromMay(avgJourneyMayFrom);
-// setAVGfromJune(avgJourneyJuneFrom);
-// setAVGfromJuly(avgJourneyJulyFrom);
-// setAVGtoMay(avgJourneyMayTo);
-// setAVGtoJune(avgJourneyJuneTo);
-// setAVGtoJuly(avgJourneyJulyTo);
-// setAvgFrom(avgJourneyLengthFrom);
-// setAvgTo(avgJourneyLengthTo);
 const StationsData = ({ coords }) => {
   const [stations, setStations] = useState([]);
   const [searchField, setSearchField] = useState('');
   const [departureNumber, setDepartureNumber] = useState();
+  const [stationName, setStationName] = useState('');
   const [arrivalNumber, setArrivalNumber] = useState();
   const [currentStationId, setCurrentStationId] = useState();
   const [avgFrom, setAvgFrom] = useState();
   const [avgTo, setAvgTo] = useState();
-  const [avgFromMay,setAVGfromMay]=useState();
-  const [avgFromJune,setAVGfromJune]=useState();
-  const [avgFromJuly,setAVGfromJuly]=useState();
-  const [avgToMay,setAVGtoMay]=useState();
-  const [avgToJune,setAVGtoJune]=useState();
-  const [avgToJuly,setAVGtoJuly]=useState();
-  const [top5Month,setTop5Month]=useState();
-  const [topReturnFromHere,setTopReturnFromHere]=useState([])
-  const [topReturnToHere,setTopReturnToHere]=useState([])
+  const [avgFromMay, setAVGfromMay] = useState();
+  const [avgFromJune, setAVGfromJune] = useState();
+  const [avgFromJuly, setAVGfromJuly] = useState();
+  const [avgToMay, setAVGtoMay] = useState();
+  const [avgToJune, setAVGtoJune] = useState();
+  const [avgToJuly, setAVGtoJuly] = useState();
+  const [top5Month, setTop5Month] = useState();
+  const [topReturnFromHere, setTopReturnFromHere] = useState([]);
+  const [topReturnToHere, setTopReturnToHere] = useState([]);
   useEffect(() => {
     availableStations();
   }, []);
@@ -47,6 +40,10 @@ const StationsData = ({ coords }) => {
     console.log(coordinates);
     coords(coordinates.x_coord, coordinates.y_coord);
     setCurrentStationId(e.target.id);
+    const stationName = coordinates.name;
+    setStationName(stationName);
+    setTopReturnFromHere([]);
+    setAvgFrom(0);
     onStationsData(e.target.id);
   };
   const onStationsData = async (fid) => {
@@ -62,7 +59,7 @@ const StationsData = ({ coords }) => {
     setDepartureNumber(numberOfDepartures);
     console.log(data.data);
   };
-  //logic to give the user the average distance from and to a station and monthly based or total 
+  //logic to give the user the average distance from and to a station and monthly based or total
   const onFurtherInfos = async () => {
     const data = await axios.get(
       `http://localhost:3000/furtherinfostation?id=${currentStationId}`
@@ -88,75 +85,71 @@ const StationsData = ({ coords }) => {
     setAvgTo(avgJourneyLengthTo);
   };
   //TOP 5 Stations
-  const onTop5Stations=async()=>{
+  const onTop5Stations = async () => {
     const data = await axios.get(
       `http://localhost:3000/top5stations?id=${currentStationId}&month=${top5Month}`
     );
     console.log(data.data);
-    const topDepartures = data.data.filter((x)=>x.departure_station_id===currentStationId);
-     const topReturns = data.data.filter(
-       (x) => x.return_station_id === currentStationId
-     );
-   const array2=[...topReturns];
-   array2.sort((a,b)=>{
-    return b.cont-a.cont;
-   });
-   setTopReturnToHere(array2)
-   const array=[...topDepartures]
-   array.sort((a,b)=>{
-    return b.cont-a.cont;
-   })
-   setTopReturnFromHere(array)
-  }
-  const months=["may","june","july"];
-  const selectMonth = (e)=>{
-    setTop5Month(e.target.value)
-  }
+    const topDepartures = data.data.filter(
+      (x) => x.departure_station_id === currentStationId
+    );
+    const topReturns = data.data.filter(
+      (x) => x.return_station_id === currentStationId
+    );
+    const array2 = [...topReturns];
+    array2.sort((a, b) => {
+      return b.cont - a.cont;
+    });
+    setTopReturnToHere(array2);
+    const array = [...topDepartures];
+    array.sort((a, b) => {
+      return b.cont - a.cont;
+    });
+    setTopReturnFromHere(array);
+  };
+  const months = ['may', 'june', 'july'];
+  const selectMonth = (e) => {
+    setTop5Month(e.target.value);
+  };
   return (
-    <div>
-      {avgFrom ? (
-        <div>
-          <div>
-            average journey distance from this station(total): {avgFrom}
-          </div>
-          <div>
-            average journey distance from this station(May): {avgFromMay}
-          </div>
-          <div>
-            average journey distance from this station(June): {avgFromJune}
-          </div>
-          <div>
-            average journey distance from this station(July): {avgFromJuly}
-          </div>
-          <div>average journey distance to this station(total): {avgTo}</div>
-          <div>average journey distance to this station(May): {avgToMay}</div>
-          <div>average journey distance to this station(June): {avgToJune}</div>
-          <div>average journey distance to this station(July): {avgToJuly}</div>
-        </div>
-      ) : null}
-      <input type="search" onChange={onSearchChange}></input>
-      {filteredStations.map((station) => {
-        return (
-          <div
-            id={station.ID}
-            key={station.FID}
-            onClick={onRetrieveStationInformations}
-          >
-            {station.name}
-            &nbsp; {station.address}
-          </div>
-        );
-      })}
+    <div className="stationsdata_container">
+      <div className="stationsdata_input">
+        <input
+          type="search"
+          onChange={onSearchChange}
+          placeholder="search stations"
+        ></input>
+      </div>
+      <div className="stationsdata">
+        {filteredStations.map((station) => {
+          return (
+            <div
+              className="stationsdata_data"
+              key={station.FID}
+              id={station.ID}
+              onClick={onRetrieveStationInformations}
+            >
+              <div className="stationsdata_data-name">{station.name}</div>
+              <div className="stationsdata_data-address">
+                {' '}
+                {station.address}
+              </div>
+            </div>
+          );
+        })}
+      </div>
       {arrivalNumber ? (
-        <div>
-          <div>
-            total numbers of journeys from this station: {departureNumber}
+        <div className="stationsdata_container-arrival">
+          <div className="stationsdata_container-arrival-header">
+            <h1>{stationName} in numbers</h1>
           </div>
           <div>
-            total numbers of journeys to this station:
-            {arrivalNumber}
+            From {stationName} {departureNumber} journeys have been departing
+            and {arrivalNumber} journeys finished to {stationName}
           </div>
-          <div onClick={onFurtherInfos}>further station infos</div>
+          <div onClick={onFurtherInfos}>
+            see the average distance of journeys starting and ending here
+          </div>
           <div>
             TOP 5 stations from and to this station in{' '}
             <select onChange={selectMonth}>
@@ -171,30 +164,68 @@ const StationsData = ({ coords }) => {
             </select>
             <button onClick={onTop5Stations}>ok</button>
           </div>
-          {topReturnFromHere ? (
+          {topReturnFromHere.length > 1 ? (
             <div>
               <div>
-                top 5 routes from here in {top5Month}
+                top 5 return stations from here in {top5Month}
                 {topReturnFromHere.map((station) => {
                   return (
                     <div>
-                      <div>{station.return_station}</div>
-                      <div>{station.cont}</div>
+                      <div>
+                        To {station.return_station}, {station.cont} times
+                      </div>
                     </div>
                   );
                 })}
               </div>
 
               <div>
-                top 5 routes to here in {top5Month}
+                top 5 departure stations that ended here in {top5Month}
                 {topReturnToHere.map((station) => {
                   return (
                     <div>
-                      <div>{station.departure_station}</div>
-                      <div>{station.cont}</div>
+                      <div>
+                        From {station.departure_station}, {station.cont} times
+                      </div>
                     </div>
                   );
                 })}
+              </div>
+            </div>
+          ) : null}
+          {avgFrom ? (
+            <div>
+              <div>
+                average journey distance from this station(total):{' '}
+                {Math.round(avgFrom) / 1000}
+              </div>
+              <div>
+                average journey distance from this station(May):{' '}
+                {Math.round(avgFromMay) / 1000}
+              </div>
+              <div>
+                average journey distance from this station(June):{' '}
+                {Math.round(avgFromJune) / 1000}
+              </div>
+              <div>
+                average journey distance from this station(July):{' '}
+                {Math.round(avgFromJuly) / 1000}
+              </div>
+              <div>
+                average journey distance to this station(total):{' '}
+                {Math.round(avgTo) / 1000}
+              </div>
+              <div>
+                average journey distance to this station(May):{' '}
+                {Math.round(avgToMay) / 1000}
+              </div>
+              <div>
+                average journey distance to this station(June):{' '}
+                {Math.round(avgToJune) / 1000}
+              </div>
+              <div>
+                average journey distance to this station(July):{' '}
+                {Math.round(avgToJuly) / 1000}
               </div>
             </div>
           ) : null}
