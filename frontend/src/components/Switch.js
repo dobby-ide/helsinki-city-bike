9c119bc8dd3ea870d10622623bf17dece530c68d
Fabio Privitera <fabio_privitera@yahoo.it>
2022-07-27 12:05:31 +0300

    cleanse some unused state, adds classNames

--- frontend/src/components/Switch.js
+++ frontend/src/components/Switch.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import { useState } from 'react';
-//Switch.js simply passes props to parent App.js and deceide which "route" to go for
+
+//Switch.js simply passes props to parent App.js and decide which "route" to go for
 const Switch = ({ initialchoice }) => {
-  const [journeySearch, setJourneySearch] = useState(false);
+ 
   const onStarting = (e) => {
     initialchoice(e.target.id);
-    // if (e.target.id === 'journey') {
-    //   setJourneySearch(true);
-    // } else {
-    //   initialchoice(e.target.id);
-    // }
-  };;
+  };
   return (
     <div className="switchcontainer">
-      <div id="sort" onClick={onStarting}>
+      <div id="sort" onClick={onStarting} className="switchcontainer_sort">
         Journeys
       </div>
-
-      <div id="singlestation" onClick={onStarting}>
+      <div
+        id="singlestation"
+        onClick={onStarting}
+        className="switchcontainer_station">
         Single Station View
       </div>
     </div>
