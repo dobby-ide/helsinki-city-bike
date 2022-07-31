528a685b22eb029576071ed85c8cdd4a838229c6
Fabio Privitera <fabio_privitera@yahoo.it>
2022-07-31 11:43:17 +0300

    serving a frontend folder as static

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const port = process.env.PORT || 3000;
 const database = require('./controller/controller_db');
 app.use(cors());
+app.use(express.static('frontend/build'));
 app.use(express.json());
 //connecting
 app.listen(port, () => {
